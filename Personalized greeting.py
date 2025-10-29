5043d6ba53471bcaa7637764309cc487871a8f9b
Lata2118 <lata21188586@gmail.com>
2025-10-30 00:33:37 +0530

    Add files via upload

---
+++ Personalized greeting.py
@@ -0,0 +1,13 @@
+"""
+A Python program that
+1. Takes the first and last name as inputs
+2. Concatenates the first and last name into a full name
+3. Prints a personalized greeting using the full name
+
+"""
+fn=input("Enter your first name: ")
+ln=input("Enter your last name: ")
+c=fn + ln
+print("Hello,", fn,"",ln, "! Welcome to Python Programming")
+
+
