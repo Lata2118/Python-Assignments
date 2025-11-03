94e183228887dec548945429e45c693a21f6a108
Lata2118 <lata21188586@gmail.com>
2025-11-03 22:41:16 +0530

    Add files via upload

---
+++ Even or odd.py
@@ -0,0 +1,11 @@
+"""
+Assigment 2
+Task 1 : A python program that takes an integer input from the user and checks if the number is even or odd
+Then displays the result accordingly
+"""
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num,"is an Even number")
+else:
+    print(num,"is an Odd number")
+
---
+++ Sum of integers.py
@@ -0,0 +1,10 @@
+"""
+Assignment 2
+Task2: A python program to find the sum of integers from 1 to 50 using a loop
+"""
+
+
+sum=0
+for i in range(1,51):
+        sum = (sum+i)
+print("Sum of numbers from 1 - 50 is :",sum)
