d2a12631ca448beef59a9451b1688e3d95d2d17f
Lata2118 <lata21188586@gmail.com>
2025-11-17 13:27:26 +0530

    Add files via upload
    Assignment-3, Task -2 A python program to calculate square root, natural logarithm an d sine of a number by taking an input from the user ( using math module)

---
+++ math_module.py
@@ -0,0 +1,14 @@
+"""
+Assignment 3
+Task-2
+A python program to calculate the following using the user input
+* Square root
+*Natural logarithm
+*sine of the number
+"""
+import math
+
+num=int(input("Enter a number:"))
+print(f"The square root of {num} is {math.sqrt(num)}")
+print(f"Natural Logarithm of {num} is {math.log(num)}")
+print(f"The Sine of {num} is {math.sin(num)}")
